1835183d9a31945e904b543ab8042156b2faf3f5
ShashankSagriNayak <144464716+ShashankSagriNayak@users.noreply.github.com>
2024-10-27 19:09:07 +0530

    Updated code

--- SER_app.py
+++ SER_app.py
@@ -13,14 +13,24 @@ resnet_model = load_model('resnet_model.h5')
 emotion_labels = ['Anger','Disgust','Fear','Happiness','Neutral','Sadness']  
 
 # Preprocess the audio
-def preprocess_audio(file, sr=16000, n_mfcc=40):
+def preprocess_audio(file, sr=16000, n_mfcc=40 ,n_mels=128):
     y, sr = librosa.load(file, sr=sr)
     mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
     delta_mfcc = librosa.feature.delta(mfcc)
     delta2_mfcc = librosa.feature.delta(mfcc, order=2)
     mfcc_features = np.vstack([mfcc, delta_mfcc, delta2_mfcc])
     mfcc_scaled = np.mean(mfcc_features.T, axis=0)
-    return np.expand_dims(mfcc_scaled, axis=0)
+    # Extract Mel-spectrogram features
+    mel_spect = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels)
+    mel_scaled = np.mean(librosa.power_to_db(mel_spect).T, axis=0)
+
+    # Combine MFCC and Mel features
+    combined_features = np.concatenate([mfcc_scaled, mel_scaled])
+    
+    # Reshape for model input (1, feature_length, 1)
+    combined_features = np.expand_dims(combined_features, axis=0)
+    combined_features = np.expand_dims(combined_features, axis=-1)
+    return combined_features
 
 # Predict emotion
 def predict_emotion(file_path, model):
@@ -37,7 +47,7 @@ if uploaded_file is not None:
     st.audio(uploaded_file, format='audio/wav')
 
     # Select model
-    model_option = st.selectbox('Choose a model', ('BiLSTM', 'CRNN', 'Transformer', 'ResNet'))
+    model_option = st.selectbox('Choose a model', ('BiLSTM', 'CRNN', 'ResNet'))
 
     if st.button('Predict'):
         if model_option == 'BiLSTM':
